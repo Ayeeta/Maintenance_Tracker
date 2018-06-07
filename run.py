82bdaa30385ac63c0621edb155a58ccb194a8fca
Ayeeta <elijate@gmail.com>
2018-06-07 14:27:10 +0300

    Add error message to view

--- run.py
+++ run.py
@@ -42,10 +42,10 @@ def login_usr():
         
     result = usr.login(info['id_no'], info['upassword'])
     if result == []:
-        return make_response('Could not verify',401,{'WWW-Authenticate':'Basic realm="Login required"'})
+        return make_response('Unauthorized',401,{'WWW-Authenticate':'Basic realm="Login required"'})
     else:
         token = jwt.encode({'id_no':info['id_no'], 'exp':datetime.datetime.utcnow() + datetime.timedelta(minutes=40)},app.config['SECRET_KEY'])
-        return jsonify({'token':token.decode('UTF-8')})
+        return jsonify({'token':token.decode('UTF-8')}),200
     
 
 @app.route('/requests/', methods=['GET'])
@@ -58,6 +58,7 @@ def create_req(current_user):
     info = request.get_json()
     usr.create_request(info['prob_title'], info['prob_desc'],info['req_type'],info['id_no'])
     return jsonify({"Message":"saved successfully"}), 200
+    
 
 @app.route('/users/requests/<prob_id>',methods=['GET'])
 @token_required
@@ -72,12 +73,16 @@ def getUReq(current_user):
 @app.route('/users/requests/<prob_id>', methods=['PUT'])
 @token_required
 def modify_req(current_user, prob_id):
-    info = request.get_json()
-    usr.modify_request(prob_id, info['prob_title'], info['prob_desc'])
-    return jsonify({"Message":"Edit successful"}),200
+    try:
+        info = request.get_json()
+        usr.modify_request(prob_id, info['prob_title'], info['prob_desc'])
+        return jsonify({"Message":"Edit successful"}),200
+    except:
+        return jsonify({"Message":"Not modified"}), 304
 
 @app.route('/requests/<prob_id>/approve', methods=['PUT'])
 def approve_req(prob_id):
+    
     usr.approve_request(prob_id)
     return jsonify({"Message":"Approve successful"}),200
 
@@ -89,14 +94,8 @@ def disapprove_request(prob_id):
 @app.route('/requests/<prob_id>/resolve', methods=['PUT'])
 def resolve_req(prob_id):
     usr.resolved_request(prob_id)
-    return jsonify({"Message":"Request Done.."}),200
-
-
+    return jsonify({"Message":"Request Done.."}),200 
     
-    
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)    
